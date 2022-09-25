9d0778aedcf593727d974fc88b351756ee5c9b0e
monicahardt <monha@itu.dk>
2022-09-25 18:23:23 +0200

    not pretty, needs lost message check

--- Try.go
+++ Try.go
@@ -40,7 +40,7 @@ func client(ch chan packet, dataCh chan dataPacket){
 
 		firstDataPacket := dataPacket{"I am a data string",0}
 		secondDataPacket := dataPacket{"I am a data string1",1}
-		thirdDataPacket := dataPacket{"I am a data string2",2}
+		thirdDataPacket := dataPacket{"I am a data string2",4}
 		dataCh <- firstDataPacket
 		dataCh <- thirdDataPacket
 		dataCh <- secondDataPacket
@@ -49,6 +49,8 @@ func client(ch chan packet, dataCh chan dataPacket){
 }
 
 	func server(ch chan packet, dataCh chan dataPacket){
+	dataArray := []dataPacket{}
+
 	//step 1: the server recieves the first packet.
 	recievedSynPacket := <- ch
 	toSendSynAckPacket := recievedSynPacket;
@@ -66,24 +68,19 @@ func client(ch chan packet, dataCh chan dataPacket){
 		recievedAckPacket.syn = 0;
 		fmt.Println("Connection established")
 		
-		dataArray := []dataPacket{}
-
-		
 		for i:=0; i < 3; i++{
 			recievedData := <- dataCh
 			fmt.Println("added to the dataArray the string: ", recievedData.data)
 			dataArray = append(dataArray, recievedData)
 		}
 
+		//sorting the slice on the metaData number to get messages right order even if sent wrong
+		sort.Slice(dataArray, func(i, j int) bool { return dataArray[i].metaData < dataArray[j].metaData})
 
-		sort.Slice(dataArray, func(i, j int) bool {
-			return dataArray[i].metaData < dataArray[j].metaData
-		})
+		//printing the data to the console
+		for s := range dataArray {fmt.Println(dataArray[s].data)}
 
-		for s := range dataArray{
-			fmt.Println(dataArray[s].data)
-		}
-		
+		//we need a check to see if there are any messages lost 
 	} else {
 		fmt.Println("Something went wrong the recieved seq is not correct")
 	}
